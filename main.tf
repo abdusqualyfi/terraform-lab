dead804faaf00281b6cfbdb81cf5357f9ba2ef5d
abdusqualyfi <118817507+abdusqualyfi@users.noreply.github.com>
2023-05-15 12:32:56 +0100

    adjusted main.tf for new devops connection

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.55.0"
+      version = ">=3.52.0"
     }
   }
 }
@@ -11,18 +11,12 @@ terraform {
 #Provides config details for Azure Terraform provider
 provider "azurerm" {
   features {}
-  use_msi = true
 }
 
 #Provides the Resource Group to logically contain resources
 resource "azurerm_resource_group" "gen_resource_group" {
-  name     = "abdus-terraform"
-  location = "eastus"
-  tags = {
-    environtment = "dev"
-    source       = "Terraform"
-    owner        = "abdus"
-  }
+  name     = "tf-lab"
+  location = "northeurope"
 }
 
 #Generate Data Factory resource
@@ -46,7 +40,7 @@ resource "azurerm_storage_account" "gen_data_lake_gen2" {
 }
 
 resource "azurerm_storage_container" "gen_storage_container" {
-  name                  = "my-container"
+  name                  = "abdus-container"
   storage_account_name  = azurerm_storage_account.gen_data_lake_gen2.name
   container_access_type = "private"
 }
@@ -65,8 +59,8 @@ resource "azurerm_sql_server" "gen_sql_server" {
   resource_group_name          = azurerm_resource_group.gen_resource_group.name
   location                     = azurerm_resource_group.gen_resource_group.location
   version                      = "12.0"
-  administrator_login          = var.db_username
-  administrator_login_password = var.db_password
+  administrator_login          = "sqladmin"
+  administrator_login_password = "Y0unWkow$22"
 
   tags = {
     environment = "dev"
@@ -74,7 +68,7 @@ resource "azurerm_sql_server" "gen_sql_server" {
 }
 
 resource "azurerm_sql_database" "gen_sql_db" {
-  name                             = "my-sql-database"
+  name                             = "abdus-sql-database"
   resource_group_name              = azurerm_resource_group.gen_resource_group.name
   location                         = azurerm_resource_group.gen_resource_group.location
   server_name                      = azurerm_sql_server.gen_sql_server.name
